ffa352a49468242176bf12e8aaacb27308f2311a
Tiago Cargnin Morandi <ti061905@intelbras.com.br>
2025-04-07 17:07:31 -0300

    Adicinado o teste para verificar se há algum navio desconhecido no tabuleiro e organizado o código em funções, para uma melhora de leitura do código.

--- exercicio-4/app/src/main/java/org/example/App.java
+++ exercicio-4/app/src/main/java/org/example/App.java
@@ -16,68 +16,92 @@
  * Exemplo: tabuleiro[linha_atual][i] = elementos[i].charAt(0); // Vai percorrer a primeira linha fixa e variar a coluna da matriz, adicionando o caracter
  * charAt vai obtér o primeiro valor da string, como são sempre um caracter, vai sempre atribuir aquela carecter;
  * 
- * 
+ * Para a segunda parte do atividade, onde é preciso verificar se há um navio desconhecido, como vamos ler a linha por linha do tabuleiro, podemos verificar se há um navio desconhecido.
+ * Feito um switch, onde cada caso é um navio conhecido, e o default é o navio desconhecido;
+ * uma variável recebe os valores de elementos[i].charAt(0), e em um switch, verifica se o valor é um navio conhecido ou não;
  */
 
+ package org.example;
+ import java.util.Scanner;
  
-package org.example;
-import java.util.Scanner;
+ public class App {
+     static final int tamanho = 10; // Define uma variável global
 
-public class App {
-    static final int tamanho = 10; // Define uma variável global
-    public static void main(String[] args) {
-        Scanner teclado_in = new Scanner(System.in); // Scanner iniciado
-
-        char[][] tabuleiro = new char[tamanho][tamanho]; // Matriz tabuleiro de 10X10, que vai guardar os caracteres do tabuleiro
-        int linhas_lidas = 0; // vai existir uma variável que será responsável em verificar se a quatidade de linhas lidas no tabuleiro estão na dimensão correta
-
-        while(teclado_in.hasNextLine()) { // Um while que vai ficar no loop até ler todas as linhas do arquivo
-            if(linhas_lidas >= tamanho) { // linhdas_lidas é inciada com 0, então quando chegar em 10 valores, vai possuir um valor inteiro igual a 9;
-                System.out.println("Tabuleiro inválido"); // If verifica se não há execessos de linhas
-            }
-            
-            String linha = teclado_in.nextLine().trim(); // A string linha, recebe a linha de entrada do arquvio junto da função trim, que tira qualquer espaço no começo e fora do arquivo
-            
-            String[] elementos = linha.split("\\s+"); // Como no atividade 3, os pontos e os navios são separados por espaços, foi pesquisado uma função para retirar os espaços
-            
-            // "\\s+" Vai separar a string onde houver espaço branco, então a string elementos, vai compor todos os elementos da linha:
-            /* Exemplo: [* * * * * * N N * *]
-                Usando o comando do split, a string elementos vai receber: elemento = ['*','*','*','*','*','*','N','N','*','*']
-                    Todos os elementos, sem conter o espaço
-            */
-            if(elementos.length != tamanho){ // Se a quantidade de linhas for diferente de 10(tamanho) já sabemos que está fora da dimensão;
-                System.out.println("Tamnaho inválido, tabuleiro fora da dimensão correta!");
-                return;
-            }
-
-            for(int i = 0; i < tamanho; i++){
-                tabuleiro[linhas_lidas][i] = elementos[i].charAt(0); 
-            /* Considerando a tabela:
-             *    1 2 3 4 5 6 7 8 9 10  - No primeiro loop do while, linhas_lidas é 0, então vai levar a primeira linha;
-             *  1 . N N . . . . . . .   - i é inicia igual a 0, então vai ler a primeira coluna
-             *  2 . . . . . . N N . .   - Tabuleiro que é uma matriz 10X10, vai receber na primeira linha coluna 1, o primeiro elemento da string "elementos", pois foi usado a função
-             *  3 . . . . . . . . . .       charAt(0) que retorna o primeiro valor;
-             *  4 . . . . . . . . . .   - Quando ler a linha toda da matriz, vai sair do for e icrementar a variável linhas_lidas, começando na proxima iteração na segunda linha; 
-             *  5 P P P P P . . . . .
-             *  6 . . . . E E E E . .
-             *  7 . . . . . . . . . .
-             *  8 . . . . . . . . . .
-             *  9 . . . . . . . . . .
-             * 10 . . . . . . . . . .
+ 
+     static boolean lerTabuleiro(Scanner teclado_in, char[][] tabuleiro) {
+         int linhas_lidas = 0; // vai existir uma variável que será responsável em verificar se a quatidade de linhas lidas no tabuleiro estão na dimensão correta
+ 
+         while(teclado_in.hasNextLine()) { // Um while que vai ficar no loop até ler todas as linhas do arquivo
+             if(linhas_lidas >= tamanho) { // linhdas_lidas é inciada com 0, então quando chegar em 10 valores, vai possuir um valor inteiro igual a 9;
+                 System.out.println("Tabuleiro inválido"); // If verifica se não há execessos de linhas
+                 return false;
+             }
+ 
+             String linha = teclado_in.nextLine().trim(); // A string linha, recebe a linha de entrada do arquvio junto da função trim, que tira qualquer espaço no começo e fora do arquivo
+             String[] elementos = linha.split("\\s+"); // Como no atividade 3, os pontos e os navios são separados por espaços, foi pesquisado uma função para retirar os espaços
+ 
+             // "\\s+" Vai separar a string onde houver espaço branco, então a string elementos, vai compor todos os elementos da linha:
+             /* Exemplo: [* * * * * * N N * *]
+                 Usando o comando do split, a string elementos vai receber: elemento = ['*','*','*','*','*','*','N','N','*','*']
+                     Todos os elementos, sem conter o espaço
              */
-            }
-            linhas_lidas++;
-        }
-
-        if(linhas_lidas == tamanho){
+             if(elementos.length != tamanho){ // Se a quantidade de linhas for diferente de 10(tamanho) já sabemos que está fora da dimensão;
+                 System.out.println("Tamnaho inválido, tabuleiro fora da dimensão correta!");
+                 return false;
+             }
+ 
+             for(int i = 0; i < tamanho; i++){ // Adicionado no for, uma verificaçã do digito, então caso não seja um ponto ou navio, imprime que o tabuleiro é inválido
+                 char c = elementos[i].charAt(0); // A variável c vai receber o primeiro elemento da string, que é o caracter, ou seja, o ponto ou navio
+                 switch(c){ // Todos os casos do switch vão ser adicionados no tabuleiro;
+                     case '.':
+                     case 'P':
+                     case 'E':
+                     case 'C':
+                     case 'S':
+                     case 'N':
+                         tabuleiro[linhas_lidas][i] = c;
+                         break; // Se for um ponto ou navio, não faz nada, só continua o loop 
+                     default:
+                         System.out.println("Tabuleiro inválido, contém caracteres inválidos!"); // Se não for nenhum dos valores válidos, imprime que o tabuleiro é inválido
+                         return false;
+                 }
+             /* Considerando a tabela:
+              *    1 2 3 4 5 6 7 8 9 10  - No primeiro loop do while, linhas_lidas é 0, então vai levar a primeira linha;
+              *  1 . N N . . . . . . .   - i é inicia igual a 0, então vai ler a primeira coluna
+              *  2 . . . . . . N N . .   - Tabuleiro que é uma matriz 10X10, vai receber na primeira linha coluna 1, o primeiro elemento da string "elementos", pois foi usado a função
+              *  3 . . . . . . . . . .       charAt(0) que retorna o primeiro valor;
+              *  4 . . . . . . . . . .   - Quando ler a linha toda da matriz, vai sair do for e icrementar a variável linhas_lidas, começando na proxima iteração na segunda linha; 
+              *  5 P P P P P . . . . .
+              *  6 . . . . E E E E . .
+              *  7 . . . . . . . . . .
+              *  8 . . . . . . . . . .
+              *  9 . . . . . . . . . .
+              * 10 . . . . . . . . . .
+              */
+             }
+             linhas_lidas++;
+         }
+ 
+         return linhas_lidas == tamanho;
+     }
+ 
+     static void imprimirTabuleiro(char[][] tabuleiro) {
+         for(int i = 0; i < tamanho; i++){
+             for(int j = 0; j < tamanho; j++){
+                 System.out.print(tabuleiro[i][j] + " "); // Vai imprimir o tabuleiro separando os elementos com " "(espaço)
+             }
+             System.out.println();
+         }
+     }
+      
+     public static void main(String[] args) {
+        Scanner teclado_in = new Scanner(System.in); // Scanner iniciado
+        char[][] tabuleiro = new char[tamanho][tamanho]; // Matriz tabuleiro de 10X10, que vai guardar os caracteres do tabuleiro
+        
+        if (lerTabuleiro(teclado_in, tabuleiro)) {
             System.out.println("Tabuleiro está correta");
-        }
-        for(int i = 0; i < tamanho; i++){
-            for(int j = 0; j < tamanho; j++){
-                System.out.print(tabuleiro[i][j] + " "); // Vai imprimir o tabuleiro separando os elementos com " "(espaço)
-            }
-            System.out.println();
+            imprimirTabuleiro(tabuleiro);
         }
     }
-
-}
+ }
+ 
